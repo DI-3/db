f0779f10f4faa1241c6b7d34a0a1c61e301b7c2e
Mithun <mitun311@gmail.com>
2023-02-03 01:04:21 -0500

    First checkin for database

---
+++ DDL/database_schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE reuniondb;
+
+CREATE TABLE users (
+    contact_id uuid DEFAULT uuid_generate_v4 (),
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    phone VARCHAR,
+    PRIMARY KEY (contact_id)
+);
