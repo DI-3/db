40b86a613795ea373c723dd1440d85fa30af23ea
Mithun <mitun311@gmail.com>
2023-02-08 18:22:25 -0500

    Sample Database and Tables

--- DDL/database_schema.sql
+++ DDL/database_schema.sql
@@ -1,6 +1,12 @@
-CREATE DATABASE reuniondb;
+CREATE DATABASE reunion;
+\connect reunion;
 
-CREATE TABLE users1 (
+create schema reunion_db
+
+grant usage on schema reunion_db to root;
+grant create on schema reunion_db to root;
+
+CREATE TABLE reunion_db.reunion_users (
     contact_id uuid,
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
@@ -10,3 +16,8 @@ CREATE TABLE users1 (
 );
 
 
+-- How to run this in psql shell
++
++--"psql -h 127.0.0.1 -p 5432 < /home/projects/web/My_App/backend/setup.sql"
++
++--sudo -U postgres bash -c "psql -h 127.0.0.1 -p 5432 -d db_name < /home/projects/web/My_App/backend/db_name.sql"
